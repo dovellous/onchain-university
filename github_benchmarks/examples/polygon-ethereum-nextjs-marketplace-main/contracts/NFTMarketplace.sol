0fdc98c93b9c8998b8dacc06e491d9f60bb41d31
dovellous <dovelous@gmail.com>
2023-03-24 10:21:46 +0200

    Updated the NFT Factory file

--- github_benchmarks/examples/polygon-ethereum-nextjs-marketplace-main/contracts/NFTMarketplace.sol
+++ github_benchmarks/examples/polygon-ethereum-nextjs-marketplace-main/contracts/NFTMarketplace.sol
@@ -8,11 +8,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
+  
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
 
     uint256 listingPrice = 0.025 ether;
+
     address payable owner;
 
     mapping(uint256 => MarketItem) private idToMarketItem;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,40 +1,129 @@
-import { config as dotEnvConfig } from "dotenv";
-dotEnvConfig();
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "@typechain/hardhat";
-import "@openzeppelin/hardhat-upgrades";
-import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
-import "./tasks/accounts";
-import "./tasks/balance";
-import "./tasks/block-number";
-import { HardhatUserConfig } from "hardhat/types";
+interface SolidityTypes {
+	compilers: any,
+	overrides: any,
+	optimizer: any
+}
 
-const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.4",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: "0.6.12",
-      },
-    ],
-  },
-  mocha: {
-    timeout: 200000,
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+const SOLIDITY: SolidityTypes = {
+	compilers: [
+		{
+			version: process.env.SOLC_VERSION,
+			settings: {
+				optimizer: {
+					enabled: process.env.OPTIMIZER_ENABLED === '1',
+					runs: process.env.OPTIMIZER_RUNS,
+				},
+			},
+		},
+	],
+	overrides: {
+		"./src/contracts/ERC1155SmartContract.sol": {
+			version: process.env.SOLC_VERSION,
+			settings: {
+				optimizer: {
+					enabled: process.env.OPTIMIZER_ENABLED === '1',
+					runs: process.env.OPTIMIZER_RUNS,
+				},
+			},
+		},
+	},
+	optimizer: {
+		enabled: process.env.OPTIMIZER_ENABLED === '1',
+		runs: process.env.OPTIMIZER_RUNS,
+	},
 };
 
-module.exports = config;
+interface PathsTypes {
+	sources: any,
+	tests: any,
+	cache: any,
+	artifacts: any
+}
+
+const PATHS: PathsTypes = {
+	sources: process.env.CONTRACT_DIR_SOURCES+"/"+process.env.CONTRACT_NAME+"/",
+	tests: process.env.CONTRACT_DIR_DEPLOYMENTS_HARDHAT+'/'+process.env.CONTRACT_NAME+"/"+process.env.CONTRACT_DIR_TESTS,
+	cache: process.env.CONTRACT_DIR_DEPLOYMENTS_HARDHAT+'/'+process.env.CONTRACT_NAME+"/"+process.env.CONTRACT_DIR_CACHE,
+	artifacts: process.env.CONTRACT_DIR_DEPLOYMENTS_HARDHAT+'/'+process.env.CONTRACT_NAME+"/"+process.env.CONTRACT_DIR_ARTIFACTS,
+};
+
+interface EtherscanTypes {
+	apiKey: string | any
+}
+
+const ETHERSCAN: EtherscanTypes = {
+	apiKey:
+		process.env.BLOCKCHAIN === "ETH"
+			? process.env.ETHERSCAN_API_KEY
+			: process.env.BLOCKCHAIN === "BSC"
+			? process.env.BSCSCAN_API_KEY
+			: process.env.POLYGON_API_KEY
+}
+
+interface MochaType {
+	timeout: any
+}
+
+const MOCHA: MochaType = {
+	timeout: process.env.MOCHA_TIMEOUT
+}
+
+interface GasReporterTypes {
+	enabled: any,
+	currency: any
+}
+
+const GAS_REPORTER: GasReporterTypes = {
+	enabled: process.env.REPORT_GAS !== undefined,
+	currency: process.env.REPORT_GAS_CURRENCY,
+}
+
+const NETWORKS: any = {
+		localhost: {
+			url: process.env.URL_LOCAL_HOST_URL + ":" + process.env.URL_LOCAL_PORT,
+		},
+		goerli: {
+			url: process.env.URL_ETH_GOERLI + process.env.INFURA_API_KEY || "",
+			chainId: 3,
+			accounts:
+				process.env.DEPLOYMENT_ACCOUNT_KEY !== undefined
+					? [process.env.DEPLOYMENT_ACCOUNT_KEY]
+					: [],
+		},
+		mainnet: {
+			url: process.env.URL_ETH_MAINNET + process.env.INFURA_API_KEY || "",
+			chainId: 1,
+			accounts:
+				process.env.DEPLOYMENT_ACCOUNT_KEY !== undefined
+					? [process.env.DEPLOYMENT_ACCOUNT_KEY]
+					: [],
+		},
+		bsc_testnet: {
+			url: process.env.URL_BSC_TESTNET,
+			chainId: 97,
+			gasPrice: 20000000000,
+			accounts: { mnemonic: process.env.MNEMONIC },
+		},
+		bsc_mainnet: {
+			url: process.env.URL_BSC_MAINNET,
+			chainId: 56,
+			gasPrice: 20000000000,
+			accounts: { mnemonic: process.env.MNEMONIC },
+		},
+	};
+
+const CONFIG: HardhatUserConfig = {
+	defaultNetwork: process.env.NETWORK,
+	networks: NETWORKS,
+	gasReporter: GAS_REPORTER,
+	solidity: SOLIDITY,
+	paths: PATHS,
+	mocha : MOCHA,
+	etherscan: ETHERSCAN,
+};
+
+module.exports = CONFIG;
